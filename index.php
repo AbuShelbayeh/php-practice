3d7b6a945142436bd901d944fee15332dbe3772c
Saleem Abu Shelbayeh <saleem.abu.shelbaya@gmail.com>
2021-04-02 20:28:44 +0300

    separation of concerns

--- index.php
+++ index.php
@@ -1,20 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- my styles -->
-  <link rel="stylesheet" href="css/style.css">
-  <title>Learn PHP | Laracasts Course</title>
-</head>
-<body>
-  <header>
-    <nav>
-      <div class="username-container"><h1>Saleem</h1></div>
-      <ul>
-        <li>Home</li>
-      </ul>
-    </nav>
-  </header>
-</body>
-</html>
+<?php
+
+$username = 'Saleem';
+
+require 'index.view.php';
---
+++ index.view.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- my styles -->
+  <link rel="stylesheet" href="css/style.css">
+  <title>Learn PHP | Laracasts Course</title>
+</head>
+<body>
+  <header>
+    <nav>
+      <div class="username-container"><h1><?= $username ?></h1></div>
+      <ul>
+        <li>Home</li>
+      </ul>
+    </nav>
+  </header>
+</body>
+</html>
