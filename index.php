d5b3325684b7062289bacf8b12a4f83bbecdc92f
Saleem Abu Shelbayeh <saleem.abu.shelbaya@gmail.com>
2021-04-02 19:07:22 +0300

    e4: add basic html to play with

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
-<?php
-
-$name = 'Saleem Abu Shelbayeh';
-$age = 22;
-echo "Hello, $name!" . "<br>Your Age is: " . $age . "<br>";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Learn PHP | Laracasts Course</title>
+</head>
+<body>
+  <header>
+    <nav></nav>
+  </header>
+</body>
+</html>
