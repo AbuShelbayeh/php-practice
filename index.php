5d12eb397b2477e67f1d1c38c1042e28222dadd8
Saleem Abu Shelbayeh <saleem.abu.shelbaya@gmail.com>
2021-04-02 19:22:52 +0300

    add stylesheet

--- index.php
+++ index.php
@@ -2,13 +2,17 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- my styles -->
+  <link rel="stylesheet" href="css/style.css">
   <title>Learn PHP | Laracasts Course</title>
 </head>
 <body>
   <header>
-    <nav></nav>
+    <nav>
+      <div class="username-container"><h1>Saleem</h1></div>
+      <ul></ul>
+    </nav>
   </header>
 </body>
 </html>
